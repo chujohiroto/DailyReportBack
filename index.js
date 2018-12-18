51321dcd6e393f0a2860a502ab4a8d1e055e9cc6
iRosSoftware <iros0522@gmail.com>
2018-12-18 21:29:44 +0900

    FIx

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.post("/sendMessage", function (req, res) {
     const done = req.body.done;
     const todo = req.body.todo;
     const trouble = req.body.trouble;
-    var message = "<https://irossoftware.github.io/DailyReportFront/|" +"Daily Report:" + date + ">" + "\n```\n" + date + "\n#やったこと\n" + done + "\n#やること\n" + todo + "\n#困ったこと\n" + trouble + "\n```"
+    var message = member + "\n<https://irossoftware.github.io/DailyReportFront/|" + date + ">" + "\n```\n" + date + "\n#やったこと\n" + done + "\n#やること\n" + todo + "\n#困ったこと\n" + trouble + "\n```"
     var senddata = JSON.stringify(
         {
         "username":member,
